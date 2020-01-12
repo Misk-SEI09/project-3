0de7aaa319069873023714b581665cfbd2a03a21
Abdulah68 <abdulahali6565@gmail.com>
2020-01-12 14:25:16 +0300

    addded state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+// import logo from './logo.svg'
+import './App.css'
+import axios from 'axios'
+import images from './Components/Data/planetsImgs'
 
-function App() {
+class  App extends Component  {
+  state = {
+    plantes: [],
+    images: []
+  }  // end state
+
+  componentDidMount(){
+    axios.get(`https://cors-anywhere.herokuapp.com/https://dry-plains-91502.herokuapp.com/planets`)
+    .then((response) =>{
+      let planets = response.data
+      //console.log(planets)
+      let copyState = {...this.state}
+      copyState.plantes = planets
+      copyState.images = images
+      this.setState(copyState)
+
+
+      // this.setState(({...copyState})=>{
+      //   copyState.plantes = response.data.plan
+      //   return copyState
+      // })
+
+      console.log(this.state)
+    })
+    .catch(function(error) {
+      console.log("Got error",error)
+    })
+    
+  } // end componet didmount
+
+  render(){
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      
 
-export default App;
+    </div>
+  )// end return
+} // end rendder
+} // end class App
+
+export default App
---
+++ src/Components/Data/planetsImgs.js
@@ -0,0 +1,56 @@
+
+let images = [
+         
+    {
+        id: 1,
+        url: 'https://image.businessinsider.com/5c9a18b8c6cc50382d262f62?width=1300&format=jpeg&auto=webp'
+    },
+
+    {
+        id: 2,
+       url: 'https://image.businessinsider.com/54a2cc11ecad043906ce4466'
+    },
+
+    {
+        id: 3,
+        url: 'https://scx1.b-cdn.net/csz/news/800/2014/1-earth.jpg'
+    },
+
+    {
+        id: 4,
+        url: 'https://spaceplace.nasa.gov/review/all-about-the-moon/the-moon-near-side.en.jpg'
+    },
+
+    {
+        id: 5,
+        url: 'https://cdn.mos.cms.futurecdn.net/L868HCTDT8ueAXXrcyXoWc-970-80.jpg'
+    },
+
+    {
+        id: 6,
+        url: 'https://en.es-static.us/upl/2019/09/jupiter-juno-2-12-2019-e1567611570663.jpg'
+    },
+
+    {
+        id: 7,
+        url: 'https://carnegiescience.edu/sites/carnegiescience.edu/files/Saturn_NASA.jpg'
+    },
+
+    {
+        id: 8,
+        url: 'https://upload.wikimedia.org/wikipedia/commons/3/3d/Uranus2.jpg'
+    },
+
+    {
+        id: 9,
+        url: 'https://www.universetoday.com/wp-content/uploads/2011/07/HubbleNeptune.jpg'
+    },
+
+    {
+        id: 10,
+        url: 'https://nineplanets.org/wp-content/uploads/2019/11/Pluto_in_True_Color-1024x1024.jpg'
+    }
+
+]
+
+export default images
