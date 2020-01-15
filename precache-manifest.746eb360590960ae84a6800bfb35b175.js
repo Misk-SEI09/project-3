3a1d5e57218a1bef239490c66c10d8449a0f7e32
Abdulah68 <abdulahali6565@gmail.com>
2020-01-15 16:45:17 +0300

    Updates

---
+++ precache-manifest.746eb360590960ae84a6800bfb35b175.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e76fd7202293affb74b46353a2b5bc11",
+    "url": "/project-3/index.html"
+  },
+  {
+    "revision": "1b83c61eaec4dd84967d",
+    "url": "/project-3/static/css/2.25d2dfdd.chunk.css"
+  },
+  {
+    "revision": "973bf22b1ee73107a384",
+    "url": "/project-3/static/css/main.197d9db5.chunk.css"
+  },
+  {
+    "revision": "1b83c61eaec4dd84967d",
+    "url": "/project-3/static/js/2.beb53c8f.chunk.js"
+  },
+  {
+    "revision": "1a3ba1185bf697297fa4b180bfc93f40",
+    "url": "/project-3/static/js/2.beb53c8f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "973bf22b1ee73107a384",
+    "url": "/project-3/static/js/main.1b30e73a.chunk.js"
+  },
+  {
+    "revision": "e145e9d0bf553d0388c2",
+    "url": "/project-3/static/js/runtime-main.0fa09ced.js"
+  }
+]);
