c4402af271f20dcb9167556b890a0d5b035ab575
Mashael <mashael@Mashaels-MacBook-Pro.local>
2020-01-15 12:51:19 +0300

    fixed css fonts, colors and design of planet, planets and about components

--- src/Components/Planets/Planets.jsx
+++ src/Components/Planets/Planets.jsx
@@ -9,15 +9,15 @@ class Planets extends Component {
     render() { 
         // console.log(this.props)
         return ( 
-            <div >
+            <div className="planets">
                 <br/>
                 <h3>Your Journey Gateway To Explore the Solar System Planets</h3>
                 <br/>
             <div className="planets-container" >
                  {this.props.planets.map( (planet, index) => {
                 return <div key={index} className="card">
-                    <img className= "Image" src={planet.media.imgUrl} alt="" />
-                    <h3> <Link to={`${this.props.match.path}/${planet.id}`} > {planet.name} </Link> </h3>
+                    <img className= "Img" src={planet.media.imgUrl} alt="" />
+                    <h3> <Link to={`${this.props.match.path}/${planet.id}`} style={ {color:"black" } }> {planet.name } </Link> </h3>
                     </div>
                 } )}
             </div>   
